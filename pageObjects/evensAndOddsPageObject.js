804a1c9b25d5bca8b24d1faa8a1723e3909139cd
Sarah Stehno <sjstehno@yahoo.com>
2019-08-09 15:58:16 -0600

    Checkpoint2 Project

---
+++ pageObjects/evensAndOddsPageObject.js
@@ -0,0 +1,22 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+        evenOddInput: 'input[name="evenOddInput"]',
+        evenResults: 'span[name="evenResults"]',
+        oddResults: 'span[name="oddResults"]',
+        evenOddButton: 'button[name="evenOddButton"]',
+        objectFilterInput: 'input[name="objectFilterInput"]',
+        objectFilterButton: 'button[name="objectFilterButton"]',
+        objectFilterResults: 'span[name="objectFilterResults"]',
+        nameFilterInput: 'input[id="nameFilterInput"]',
+        nameFilterButton: 'button[id="nameFilterButton"]',
+        nameFilterResults: 'span[name="nameFilterResults"]',
+        palindromeInput: 'input[name="palindromeInput"]',
+        palindromeButton: 'button[name="palindromeButton"]',
+        palindromeResults: 'span[name="palindromeResults"]',
+        sumInput1: 'input[name="sumInput1"]',
+        sumInput2: 'input[name="sumInput2"]',
+        sumButton: 'button[name="sumButton"]',
+        sumResults: 'span[name="sumResults"]'
+    },
+}
